19df1a0707f20f00f115634912dd2cf814679b09
BertoLaDK <lucasmulberg@live.dk>
2024-12-01 21:53:33 +0100

    Inital Day 1

---
+++ Day 1/part1.cpp
@@ -0,0 +1,52 @@
+//
+// Created by lucas on 01-12-2024.
+//
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <ctime>
+#include <chrono>
+
+bool descending(int x, int y){
+   return x >= y;
+}
+bool ascending(int x, int y){
+    return y >= x;
+}
+bool ascendingPairs(std::pair<int,int>a, std::pair<int,int>b){
+    return b.first >= a.first;
+}
+
+int main() {
+
+    std::ifstream file("C:\\Users\\lucas\\OneDrive\\Advent Of Code\\2024\\Day 1\\input.txt");
+    std::string str;
+    std::vector<int> firstList, secondList;
+    std::clock_t time = std::clock();
+    auto start = std::chrono::high_resolution_clock::now();
+
+    while (std::getline(file, str)){
+        auto first = std::stoi(str.substr(0, str.find("   ")));
+        firstList.push_back(first);
+        auto second = std::stoi(str.substr(str.find("   ")+2));
+        secondList.push_back(second);
+    }
+
+    std::sort(firstList.begin(), firstList.end(), ascending);
+    std::sort(secondList.begin(), secondList.end(), ascending);
+
+    int total = 0;
+
+    for (int i = 0; i < firstList.size(); ++i) {
+        total += std::abs(firstList[i] - secondList[i]);
+    }
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count();
+    std::cout << "Total: " << total << " Time: " << duration << " ms" << std::endl;
+
+
+
+    return 0;
+}
---
+++ Day 1/part2.cpp
@@ -0,0 +1,68 @@
+//
+// Created by lucas on 01-12-2024.
+//
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
+#include <chrono>
+#include <algorithm>
+
+bool ascending(int x, int y){
+    return y >= x;
+}
+int method1(const std::vector<int>& firstList, const std::vector<int>& secondList);
+int method2(const std::vector<int>& firstList, std::vector<int> secondList);
+
+int main() {
+
+    std::ifstream file("C:\\Users\\lucas\\OneDrive\\Advent Of Code\\2024\\Day 1\\input.txt");
+    std::string str;
+    std::vector<int> firstList, secondList;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    while (std::getline(file, str)) {
+        auto first = std::stoi(str.substr(0, str.find("   ")));
+        firstList.push_back(first);
+        auto second = std::stoi(str.substr(str.find("   ") + 2));
+        secondList.push_back(second);
+    }
+
+    int total = method2(firstList, secondList);
+
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count();
+    std::cout << "Total: " << total << " Time: " << duration << " microseconds" << std::endl;
+}
+
+int method1(const std::vector<int>& firstList, const std::vector<int>& secondList){
+    int total = 0;
+    for (int num : firstList) {
+        int multiplier = 0;
+        for (int j : secondList) {
+            if (j == num)
+                multiplier++;
+
+        }
+        total += num * multiplier;
+    }
+    return total;
+}
+
+int method2(const std::vector<int>& firstList, std::vector<int> secondList){
+    int total = 0;
+    std::sort(secondList.begin(), secondList.end(), ascending);
+    for (int num : firstList) {
+        int multiplier = 0;
+        int index = 0;
+        while(secondList[index] != num && index < secondList.size()) index++;
+        while(secondList[index] == num) {
+            index++;
+            multiplier++;
+            if(index >= secondList.size())
+                break;
+        }
+        total += num * multiplier;
+    }
+    return total;
+}
