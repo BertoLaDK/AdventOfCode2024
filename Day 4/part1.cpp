a3420817da46212aafb0ba7e57ecc12969771941
BertoLaDK <lucasmulberg@live.dk>
2024-12-04 17:17:07 +0100

    Finished Day 4

--- Day 4/part1.cpp
+++ Day 4/part1.cpp
@@ -6,24 +6,27 @@
 #include <fstream>
 #include <vector>
 #include <iostream>
+#include <chrono>
 
 std::vector<std::pair<int, int>>
 checkAround(int i, int j, std::vector<std::vector<char>> &wordMap, char charToCheckFor);
 
-bool checkDirection(std::pair<int, int> match, int i, int j, std::vector<std::vector<char>> &wordMap);
+bool checkDirection(std::pair<int, int> match, int i, int j, std::vector<std::vector<char>> &wordMap,
+                    std::vector<std::vector<char>> &wordMapFiltered);
 
 int main() {
 
     std::ifstream file("..\\Day 4\\input.txt");
     std::string str;
+    auto start = std::chrono::high_resolution_clock::now();
     std::vector<std::vector<char>> wordMap;
     while (std::getline(file, str)) {
         std::vector<char> line(str.begin(), str.end());
         wordMap.push_back(line);
     }
-    std::vector<std::vector<char>> wordMapCopy(wordMap.begin(),wordMap.end());
-    for (auto& line : wordMapCopy) {
-        for (char & i : line) {
+    std::vector<std::vector<char>> wordMapCopy(wordMap.begin(), wordMap.end());
+    for (auto &line: wordMapCopy) {
+        for (char &i: line) {
             i = '.';
         }
     }
@@ -33,8 +36,8 @@ int main() {
             if (wordMap[i][j] == 'X') {
                 std::vector<std::pair<int, int>> matches = checkAround(i, j, wordMap, 'M');
                 for (auto match: matches) {
-                    bool hasXmas = checkDirection(match, i, j, wordMap);
-                    if(hasXmas){
+                    bool hasXmas = checkDirection(match, i, j, wordMap, wordMapCopy);
+                    if (hasXmas) {
                         wordMapCopy[i][j] = 'X';
                         wordMapCopy[match.first][match.second] = 'M';
                         xmasCount++;
@@ -43,25 +46,36 @@ int main() {
             }
         }
     }
-    std::cout << "xmas count: " << xmasCount << std::endl;
+    for (int i = 0; i < wordMapCopy.size(); ++i) {
+        for (int j = 0; j < wordMapCopy[i].size(); ++j) {
+            std::cout << wordMapCopy[i][j];
+        }
+        std::cout << std::endl;
+    }
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count();
+    std::cout << "XMAS Count: " << xmasCount << " Time: " << duration << " ms" << std::endl;
 }
 
-bool checkDirection(std::pair<int, int> match, int i, int j, std::vector<std::vector<char>> &wordMap) {
+bool checkDirection(std::pair<int, int> match, int i, int j, std::vector<std::vector<char>> &wordMap,
+                    std::vector<std::vector<char>> &wordMapFiltered) {
     int x_vector = match.first - i;
     int y_vector = match.second - j;
-    int next_x = i+x_vector*2;
-    int next_y = j+y_vector*2;
-    if(wordMap.size() > next_x && wordMap[next_x].size() > next_y && wordMap[next_x][next_y] == 'A'){
-        next_x = 3*x_vector+i;
-        next_y = 3*y_vector+j;
-        if(wordMap.size() > next_x && wordMap[next_x].size() > next_y && wordMap[next_x][next_y] == 'S'){
+    int a_x = i + x_vector * 2;
+    int a_y = j + y_vector * 2;
+    if (wordMap.size() > a_x && wordMap[a_x].size() > a_y && wordMap[a_x][a_y] == 'A') {
+        int s_x = 3 * x_vector + i;
+        int s_y = 3 * y_vector + j;
+        if (wordMap.size() > s_x && wordMap[s_x].size() > s_y && wordMap[s_x][s_y] == 'S') {
+            wordMapFiltered[a_x][a_y] = 'A';
+            wordMapFiltered[s_x][s_y] = 'S';
             return true;
         }
     }
     return false;
 }
 
-std::vector<std::pair<int, int>> checkAround(int i, int j, std::vector<std::vector<char>> &wordMap, char charToCheckFor) {
+std::vector<std::pair<int, int>>
+checkAround(int i, int j, std::vector<std::vector<char>> &wordMap, char charToCheckFor) {
     std::vector<std::pair<int, int>> matches;
     if (i != 0) {
         int temp_i = i - 1;
@@ -69,30 +83,30 @@ std::vector<std::pair<int, int>> checkAround(int i, int j, std::vector<std::vect
             if (wordMap[temp_i][j - 1] == charToCheckFor) {
                 matches.emplace_back(temp_i, j - 1);
             }
-            if (j < wordMap[temp_i].size()) {
-                if (wordMap[temp_i][j + 1] == charToCheckFor) {
-                    matches.emplace_back(temp_i, j + 1);
-                }
-            }
-            if (wordMap[temp_i][j] == charToCheckFor) {
-                matches.emplace_back(temp_i, j);
+        }
+        if (j < wordMap[temp_i].size()) {
+            if (wordMap[temp_i][j + 1] == charToCheckFor) {
+                matches.emplace_back(temp_i, j + 1);
             }
         }
+        if (wordMap[temp_i][j] == charToCheckFor) {
+            matches.emplace_back(temp_i, j);
+        }
     }
-    if (i < wordMap.size()-1) {
+    if (i < wordMap.size() - 1) {
         int temp_i = i + 1;
         if (j != 0) {
             if (wordMap[temp_i][j - 1] == charToCheckFor) {
                 matches.emplace_back(temp_i, j - 1);
             }
-            if (j < wordMap[temp_i].size()) {
-                if (wordMap[temp_i][j + 1] == charToCheckFor) {
-                    matches.emplace_back(temp_i, j + 1);
-                }
+        }
+        if (j < wordMap[temp_i].size()) {
+            if (wordMap[temp_i][j + 1] == charToCheckFor) {
+                matches.emplace_back(temp_i, j + 1);
             }
-            if (wordMap[temp_i][j] == charToCheckFor) {
-                matches.emplace_back(temp_i, j);
-            }
+        }
+        if (wordMap[temp_i][j] == charToCheckFor) {
+            matches.emplace_back(temp_i, j);
         }
     }
     if (wordMap[i][j - 1] == charToCheckFor) {
--- Day 4/part2.cpp
+++ Day 4/part2.cpp
@@ -1,3 +1,117 @@
 //
 // Created by Lucas on 04-12-2024.
 //
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <chrono>
+
+std::vector<std::pair<int, int>>
+checkCorners(int i, int j, std::vector<std::vector<char>> &wordMap, char charToCheckFor);
+
+
+bool checkOpposite(std::pair<int, int> match, char charToMatch, int i, int j, std::vector<std::vector<char>> &wordMap,
+                   std::vector<std::vector<char>> &wordMapFiltered);
+
+int main() {
+
+    std::ifstream file("..\\Day 4\\input.txt");
+    std::string str;
+    auto start = std::chrono::high_resolution_clock::now();
+    std::vector<std::vector<char>> wordMap;
+    while (std::getline(file, str)) {
+        std::vector<char> line(str.begin(), str.end());
+        wordMap.push_back(line);
+    }
+    std::vector<std::vector<char>> wordMapFiltered(wordMap.begin(), wordMap.end());
+    for (auto &line: wordMapFiltered) {
+        for (char &i: line) {
+            i = '.';
+        }
+    }
+    int xmasCount = 0;
+    for (int i = 0; i < wordMap.size(); ++i) {
+        for (int j = 0; j < wordMap[i].size(); ++j) {
+            if (wordMap[i][j] == 'A') {
+                int matchingChars = 0;
+                std::vector<std::pair<int, int>> matches = checkCorners(i, j, wordMap, 'M');
+                for (auto match: matches) {
+                    bool hasXmas = checkOpposite(match, 'S', i, j, wordMap, wordMapFiltered);
+                    if (hasXmas) {
+                        matchingChars++;
+                    }
+                }
+                if(matchingChars == 2){
+                    wordMapFiltered[i][j] = 'A';
+                    for (auto match: matches) {
+                        wordMapFiltered[match.first][match.second] = 'M';
+                        int x_vector = match.first - i;
+                        int y_vector = match.second - j;
+                        int o_x = i + x_vector * -1;
+                        int o_y = j + y_vector * -1;
+                        wordMapFiltered[o_x][o_y] = 'S';
+                    }
+                    xmasCount++;
+                }
+            }
+        }
+    }
+//    for (int i = 0; i < wordMapFiltered.size(); ++i) {
+//        for (int j = 0; j < wordMapFiltered[i].size(); ++j) {
+//            std::cout << wordMapFiltered[i][j];
+//        }
+//        std::cout << std::endl;
+//    }
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count();
+    std::cout << "XMAS Count: " << xmasCount << " Time: " << duration << " ms" << std::endl;
+}
+
+bool checkOpposite(std::pair<int, int> match, char charToMatch, int i, int j, std::vector<std::vector<char>> &wordMap,
+                   std::vector<std::vector<char>> &wordMapFiltered) {
+    int x_vector = match.first - i;
+    int y_vector = match.second - j;
+    int o_x = i + x_vector * -1;
+    int o_y = j + y_vector * -1;
+    if(wordMap.size() > o_x && wordMap[o_x].size() > o_y){
+        if(wordMap[o_x][o_y] == charToMatch){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+std::vector<std::pair<int, int>>
+checkCorners(int i, int j, std::vector<std::vector<char>> &wordMap, char charToCheckFor) {
+    std::vector<std::pair<int, int>> matches;
+    if (i != 0) {
+        int temp_i = i - 1;
+        if (j != 0) {
+            if (wordMap[temp_i][j - 1] == charToCheckFor) {
+                matches.emplace_back(temp_i, j - 1);
+            }
+        }
+        if (j < wordMap[temp_i].size()) {
+            if (wordMap[temp_i][j + 1] == charToCheckFor) {
+                matches.emplace_back(temp_i, j + 1);
+            }
+        }
+    }
+    if (i < wordMap.size() - 1) {
+        int temp_i = i + 1;
+        if (j != 0) {
+            if (wordMap[temp_i][j - 1] == charToCheckFor) {
+                matches.emplace_back(temp_i, j - 1);
+            }
+        }
+        if (j < wordMap[temp_i].size()) {
+            if (wordMap[temp_i][j + 1] == charToCheckFor) {
+                matches.emplace_back(temp_i, j + 1);
+            }
+        }
+    }
+
+    return matches;
+}
